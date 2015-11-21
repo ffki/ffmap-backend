510a0c6cc929edba520e840160a712fc6de0e28d
ruben <localgit@r.z11.de>
2015-11-21 08:54:06 +0100

    Adapt values to ffnord

--- mkmap.sh
+++ mkmap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-SOURCE="http://vpn0.freifunk.in-kiel.de"
+SOURCE="http://vpn0.ffnord.net"
 DEST="$1"
 [ -d "$DEST" ] || exit 1
 
@@ -10,5 +10,5 @@ wget -q -O json/alfred.158.json "$SOURCE/alfred.cgi?158"
 wget -q -O json/alfred.159.json "$SOURCE/alfred.cgi?159"
 wget -q -O json/batman.json 	"$SOURCE/batman.cgi"
 
-PATH="$(dirname "$0")/bin:$PATH" ./backend.py -d $DEST --prune 999 --vpn de:ad:be:ef:ff:01
+PATH="$(dirname "$0")/bin:$PATH" ./backend.py -d $DEST --prune 999 --vpn fe:ed:be:ff:ff:00
 
